46891790c1d79b15610f9972d69d488b6dd11219
AlfonsBC <albace2000@hotmail.com>
2021-02-22 13:38:03 -0600

    Adding indez creation code

---
+++ 2_indexcreation.sql
@@ -0,0 +1,6 @@
+--Index of tables
+CREATE INDEX idx_matricula ON exalumnos(matricula);
+CREATE INDEX idx_id_carrera ON carrera(id_carrera);
+CREATE INDEX idx_rfc ON empresas(rfc);
+CREATE INDEX idx_id_sucursal ON sucursales(id_sucursal);
+CREATE INDEX idx_id_pais ON paises(id_pais);
